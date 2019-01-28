4702569c0bc08ffcbc3a43cdb0daa61d0388b9e6
rahul-go <rahulxgoyal@gmail.com>
2019-01-28 10:46:57 -0800

    Inserted answers to questions

--- Lab 1/Lab1.m
+++ Lab 1/Lab1.m
@@ -78,15 +78,11 @@ ylabel('Response (units)');
 xlim([0, 60]);
 
 %% Exercise 2 Questions
-% The system behaves as expected: it approaches a final output change of
-% 0.5 and takes one second to reach about 63.2% of this final output
-% change.
-%
-% To produce a steady-state response twice as great in the same time frame,
-% the steady-state gain can be multiplied by 2.
-%
-% To accelerate the system such that it responds twice as quickly as the
-% original system, the time constant can be divided by 2.
+% The system behaves as expected: With an input step size of 1, it approaches a final output change of 0.5 and takes one second to reach about 63.2% of this final output change.
+% 
+% To produce a steady-state response twice as great in the same time frame, the steady-state gain can be multiplied by 2.
+% 
+% To accelerate the system such that it responds twice as quickly as the original system, the time constant can be divided by 2.
 
 
 
@@ -127,12 +123,11 @@ ylabel('Response (units)');
 xlim([0, 4]);
 
 %% Exercise 3 Questions
-% Doubling the damping coefficient causes the step response to approach
-% steady-state twice as quickly.
-%
-% Doubling the oscillation frequency causes the step response to both
-% oscillate at twice the frequency and, as a side-effect, also approach
-% steady-state twice as quickly.
+% Doubling the damping coefficient causes the step response to approach steady-state twice as quickly.
+% 
+% The percent overshoot decreases compared to the original system -- it decreases from 62% to 37%.
+% 
+% Doubling the oscillation frequency causes the step response to both oscillate at twice the frequency and, as a side-effect, also approach steady-state twice as quickly. The percent overshoot remains the same as the original system.
 
 
 
@@ -163,19 +158,17 @@ ylabel('Response (units)');
 xlim([0, 30]);
 
 %% Exercise 4 Questions
-% The second-order system constituted by two first-order systems in series
-% does not oscillate. Rather, it acts like a step response.
+% The steady-state gain of the system is the product of the gains of each first-order system.
 % 
-% The two first-order systems in series results in a step-response. This
-% step-response has a gain equal to the product of the gains of each
-% subsystem and a time constant equal to the sum of the time constants of
-% each subsystem.
+% The second-order system constituted by the two first-order systems in series behaves in a similar manner to a first-order system. The response of the system does not oscillate and is similar to a first-order step response.% The two first-order systems in series results in a step-response. This
+% 
+% The response of the two different first-order systems in series resembles a first-order response more than that of the same first-order systems in series. The step-response has a gain equal to the product of the gains of each subsystem and a time constant equal to the sum of the time constants of each subsystem.
 
 
 
 %% Exercise 5
-% PID controller by itself with no plant or feedback. All gains 1. Unit
-% step which occurs at time 1 second.
+% PID controller by itself with no plant or feedback. All gains = 1. Unit
+% step which occurs at time = 1 second.
 
 figure;
 sim('Lab1_5');
@@ -184,27 +177,36 @@ sim('Lab1_5');
 subplot(4, 1, 1);
 plot(tout, simout(:, 1), 'LineWidth', 2);
 title('PID Controller');
+xlabel('Time(s)');
+ylabel('Response (units)');
 ylim([0 10]);
 
 % Proportional Control
 subplot(4, 1, 2);
 plot(tout, simout(:, 2), 'LineWidth', 2);
 title('Proportional Control');
-% TODO
+xlabel('Time(s)');
+ylabel('Response (units)');
 
 % Integral Control
 subplot(4, 1, 3);
 plot(tout, simout(:, 3), 'LineWidth', 2);
 title('Integral Control');
-% TODO
+xlabel('Time(s)');
+ylabel('Response (units)');
 
 % Derivative Control
 subplot(4, 1, 4);
 plot(tout, simout(:, 4), 'LineWidth', 2);
 title('Derivative Control');
-% TODO
+xlabel({'Time (s)'
+        ''
+        % Figure label
+        '\bfFigure 5: \rmPID Controller'});
+ylabel('Response (units)');
 
 %% Exercise 5 Questions
+% Each component of the PID controller plays to different advantages of the change in error over time. In this case, where the error is held constant, the proportional component maintains a constant value, the integral component maintains a steadily rising value, and the derivative component maintains a zero value after the initial impulse. The overall PID controller is just the sum of each component.
 
 
 
@@ -218,6 +220,8 @@ title('Derivative Control');
 % seconds. The inlet valve is a gain since the valve reacts very quickly
 % compared with the tank. Let Kv = 1 gpm/10% valve opening, so Kv = 0.10
 % gpm/%.
+
+
 
 %% Exercise 6A
 % Proportional-only controller. The input of the whole system is the
@@ -243,6 +247,15 @@ ylabel('Height (in)');
 xlim([0, 75]);
 
 %% Exercise 6A Questions
+% The step response looks like a first-order step response, however the steady-state gain is not 0.1 in as one might expect if it were an open-loop system.
+% 
+% The steady-state gain of the closed-loop system is 0.909 in, whereas the steady-state gain of the tank alone is 1 in.
+% 
+% The time constant of the closed-loop system is 13.6s, whereas the time constant of the tank alone is 15 s.
+% 
+% Although the unit step input commanded a 1 in increase in tank level, the steady-state output value is not 1 in, nor is it close.
+
+
 
 %% Exercise 6B
 % The reference input is changed to a 1” increase in tank level at t = 1
@@ -261,6 +274,9 @@ ylabel('Height (in)');
 xlim([0, 180]);
 
 %% Exercise 6B Questions
+% The response is not good enough and it is not a significant improvement over the open-loop step response (yet).
+
+
 
 %% Exercise 6C
 % The controller is to be designed to perform well with a particular plant.
@@ -278,7 +294,7 @@ xlabel({'Time (s)'
 ylabel('Height (in)');
 xlim([0, 120]);
 
-%% Exercise 6C Questions
+
 
 %% Exercise 6D
 % The gain is modified further to reduce the time constant to less than one
@@ -307,6 +323,9 @@ ylabel('Height (in)');
 xlim([74, 80]);
 
 %% Exercise 6D Questions
+% The time constant would dictate the controller gain because steady-state error can be adjusted easily with the integral component of a PID controller, but the time constant is more easily adjustable with the proportional component of a PID controller.
+
+
 
 %% Exercise 6E
 % A common problem for control systems is saturation. A real-world actuator
@@ -334,7 +353,7 @@ xlabel({'Time (s)'
 ylabel('Height (in)');
 xlim([0, 120]);
 
-%% Exercise 6E Questions
+
 
 %% Exercise 6F
 % The controller design is modified to eliminate the saturation by reducing
@@ -350,8 +369,6 @@ xlabel({'Time (s)'
         '\bfFigure 6F: \rmProportional-Only Control of a Tank: Eliminating Saturation'});
 ylabel('Height (in)');
 xlim([0, 120]);
-
-%% Exercise 6F Questions
 
 
 
@@ -385,6 +402,7 @@ xlim([0, 30]);
 
 
 %% Exercise 7 Questions
+% In both cases, the tank does not fully reach the desired value at steady-state, likely due to the smaller Kp that eliminates saturation of the valve. This leads to a steady-state error which can be fixed by enabling the integral component of the PID controller. The percent error between both cases is constant.
 
 
 
@@ -402,7 +420,7 @@ subplot(2, 1, 1);
 plot(tout, simout, 'LineWidth', 2);
 title('Proportional-Only Control of a Second-Order System: Kp = 1');
 xlabel('Time (s)');
-ylabel('Units');
+ylabel('Response (units)');
 xlim([0.75, 1.5]);
 
 % Kp = 2
@@ -414,8 +432,12 @@ xlabel({'Time (s)'
         ''
         % Figure label
         '\bfFigure 8: \rmProportional-Only Control of a Second-Order System'});
-ylabel('Units');
+ylabel('Response (units)');
 xlim([0.75, 1.5]);
 
 %% Exercise 8 Questions
-
+% The response is a second-order response determined by the steady-state gain, damping ratio, and natural frequency of the system.
+% 
+% The block diagram algebra indicates that the closed-loop parameters are as follows: a steady-state gain of 395.37, a damping ratio of 0.149, and a natural frequency of 173.05 rad/s.
+% 
+% The effect of doubling the controller gain is mostly seen in the amplitude of the response, however it also has a small effect on the natural frequency and damping ratio of the system, as expected by the block diagram algebra. According to the block diagram algebra, the closed-loop parameters are as follows: a steady-state gain of 790.74, a damping ratio of 0.148, and a natural frequency of 174.18 rad/s.
