6ab7f70509a51d35efcaf96f950a8f5c57bd0f48
rahul-go <rahulxgoyal@gmail.com>
2019-02-10 21:00:16 -0800

    File reorganization

---
+++ Lab 2/Lab2.m
@@ -0,0 +1,88 @@
+%% ME 422 Lab 2
+% Rahul Goyal
+%
+% Jake Rogers
+%
+% February 11, 2019
+
+% Reset
+clc;
+clear all;
+close all;
+
+
+
+%% Closed-Loop, Experimental and Simulated (Linear, Nonlinear, Nonlinear Tuned)
+
+figure;
+hold on;
+
+% Experimental Data
+K_pot = 0.0851;
+load('MotoMatic/stepresponse_rf5k.mat');
+plot(MotoOpenLoop(:, 1)-1.18, MotoOpenLoop(:, 2)/K_pot, 'LineWidth', 2);
+% Linear Simulation
+sim('ClosedLoopLinear');
+plot(tout, simout, 'LineWidth', 2);
+% Nonlinear Simulation
+sim('ClosedLoopNonlinear');
+plot(tout, simout, 'LineWidth', 2);
+% Nonlinear Simulation, Tuned
+sim('ClosedLoopNonlinearTuned');
+plot(tout, simout, 'LineWidth', 2);
+% Plot labeling
+title('Closed-Loop Motomatic Step Response');
+xlabel({'Time (s)'
+        ''
+        % Figure label
+        '\bfFigure 1: \rmClosed-Loop Motomatic Step Response'});
+ylabel('Angular Position (deg)');
+legend('Experimental Data', 'Linear Simulation', ...
+       'Nonlinear Simulation', 'Nonlinear Simulation, Tuned', ...
+       'Location', 'southeast');
+xlim([0, 4]);
+ylim([0, 100]);
+
+
+
+%% Open-Loop Step Response
+
+figure;
+hold on;
+
+% Experimental Data
+load('MotoMatic/stepresponse.mat');
+plot(MotoOpenLoop(:, 1)-0.53, (MotoOpenLoop(:, 2)-0.1514)*60, 'LineWidth', 2);
+% Linear Simulation
+sim('OpenLoopLinear');
+plot(tout, simout, 'LineWidth', 2);
+% Nonlinear Simulation
+sim('OpenLoopNonlinear');
+plot(tout, simout, 'LineWidth', 2);
+% Plot labeling
+title('Open-Loop Motomatric Step Response');
+xlabel({'Time (s)'
+        ''
+        % Figure label
+        '\bfFigure 2: \rmOpen-Loop Motomatic Step Response'});
+ylabel('Angular Velocity (deg/s)');
+legend('Experimental Data', 'Linear Simulation', ...
+       'Nonlinear Simulation', 'Location', 'southeast')
+xlim([0, 4]);
+
+K_pot = 0.0851;
+
+% Rf = 5k Ohms
+figure;
+load('MotoMatic/stepresponse_rf5k.mat');
+plot(MotoOpenLoop(:, 1)-1.18, MotoOpenLoop(:, 2)/K_pot, 'LineWidth', 2);
+
+% Rf = 10k Ohms
+figure;
+load('MotoMatic/stepresponse_rf10k.mat');
+plot(MotoOpenLoop(:, 1)-1.10, MotoOpenLoop(:, 2)/K_pot, 'LineWidth', 2);
+
+% Rf = 20k Ohms
+figure;
+load('MotoMatic/stepresponse_rf20k.mat');
+plot(MotoOpenLoop(:, 1)-1.03, MotoOpenLoop(:, 2)/K_pot, 'LineWidth', 2);
--- Lab 2/Lab2B.m
+++ Lab 2/Lab2B.m
@@ -1,4 +1,4 @@
-%% ME 422 Lab 2A
+%% ME 422 Lab 2B
 % Rahul Goyal
 %
 % Jake Rogers
@@ -12,59 +12,39 @@ close all;
 
 
 
-%% Closed-Loop, Experimental and Simulated (Linear, Nonlinear, Nonlinear Tuned)
-
-figure;
-hold on;
-
-% Experimental Data
-K_pot = 0.0851;
-load('MotoMatic/stepresponse_rf5k.mat');
-plot(MotoOpenLoop(:, 1)-1.18, MotoOpenLoop(:, 2)/K_pot, 'LineWidth', 2);
-% Linear Simulation
-sim('ClosedLoopLinear');
-plot(tout, simout, 'LineWidth', 2);
-% Nonlinear Simulation
-sim('ClosedLoopNonlinear');
-plot(tout, simout, 'LineWidth', 2);
-% Nonlinear Simulation, Tuned
-sim('ClosedLoopNonlinearTuned');
-plot(tout, simout, 'LineWidth', 2);
-% Plot labeling
-title('Closed-Loop Motomatic Response Simulation');
-xlabel({'Time (s)'
-        ''
-        % Figure label
-        '\bfFigure 1: \rmClosed-Loop Motomatic Step Response'});
-ylabel('Response (deg)');
-legend('Experimental Data', 'Linear Simulation', ...
-       'Nonlinear Simulation', 'Nonlinear Simulation, Tuned', ...
-       'Location', 'southeast');
-xlim([0, 4]);
-
-
 
 %% Open-Loop Step Response
 
-figure;
-hold on;
-
-% Experimental Data
 load('MotoMatic/stepresponse.mat');
-plot(MotoOpenLoop(:, 1)-0.53, (MotoOpenLoop(:, 2)-0.1514)*40, 'LineWidth', 2);
-% Linear Simulation
-sim('OpenLoopLinear');
-plot(tout, simout, 'LineWidth', 2);
-% Nonlinear Simulation
-sim('OpenLoopNonlinear');
-plot(tout, simout, 'LineWidth', 2);
-% Plot labeling
-title('Experimental Step Response');
-xlabel({'Time (s)'
-        ''
-        % Figure label
-        '\bfFigure 2: \rmOpen-Loop Motomatic Step Response'});
-ylabel('Response (deg)');
-legend('Experimental Data', 'Linear Simulation', ...
-       'Nonlinear Simulation', 'Location', 'southeast')
-xlim([0, 4]);
+plot(MotoOpenLoop(:, 1)-0.53, MotoOpenLoop(:, 2)*6, 'LineWidth', 2);
+
+low = 0.1514;
+high1 = 1.816;
+high2 = 1.709;
+high = (high1 + high2) / 2;
+
+delta = high - low;
+tc_y = 0.632 * delta;
+
+time1 = 1.54;
+time2 = 1.83;
+
+tc = time2 - time1;
+
+
+
+%% Position vs. Time (Saturated at 200 deg)
+
+K_pot = 0.0851;
+load('stepresponse_rf5k.mat');
+plot(MotoOpenLoop(:, 1)-1.18, MotoOpenLoop(:, 2)/K_pot, 'LineWidth', 2);
+load('stepresponse_rf10k.mat');
+plot(MotoOpenLoop(:, 1)-1.10, MotoOpenLoop(:, 2)/K_pot, 'LineWidth', 2);
+load('stepresponse_rf20k.mat');
+plot(MotoOpenLoop(:, 1)-1.03, MotoOpenLoop(:, 2)/K_pot, 'LineWidth', 2);
+legend('Rf = 5k Ohm', 'Rf = 10k Ohm', 'Rf = 20k Ohm');
+xlim([0, 3]);
+
+os_rf5k = (4.541 - 3.65) / 3.65;
+os_rf10k = (5.405 - 3.07) / 3.07;
+os_rf20k = (5.762 - 3.12) / 3.12;
