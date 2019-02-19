2a9b374c3e3fa69e9dcd71f70378a35749d4633b
rahul-go <rahulxgoyal@gmail.com>
2019-02-19 15:17:51 -0800

    Minor changes

--- HW 7/HW7.m
+++ HW 7/HW7.m
@@ -3,20 +3,17 @@
 %
 % February 19, 2019
 
-% Reset
-clc;
-clear all;
-close all;
+
 
 %% Problem 4
 
 G_OL = zpk([], [0, -3, -6], 1);
-rlocus(G_OL);
-rlocfind(G_OL)
+% rlocus(G_OL);
+% rlocfind(G_OL);
 
 
 
 %% Problem 5
 
 G_OL = zpk([], [0, -3, -6], 1);
-rltool(G_OL);
+% rltool(G_OL);
