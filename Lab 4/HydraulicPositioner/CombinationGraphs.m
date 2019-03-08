f043a494f08d4b97c810d226ffd6cf71ea1fab45
keyannabh <48195436+keyannabh@users.noreply.github.com>
2019-03-07 21:20:55 -0800

    Working on report

--- Lab 4/HydraulicPositioner/CombinationGraphs.m
+++ Lab 4/HydraulicPositioner/CombinationGraphs.m
@@ -5,7 +5,7 @@ hold on
 plot(at,ameas_pos,'r:','LineWidth',2)
 plot(tout,simout_9a,'b')
 legend('Experimental','Model','Location','best')
-title('9(a) PD [10 0 0.01] Step Input')
+title('PD [10 0 0.01] Step Input')
 ylabel('Displacement (inches)')
 xlabel('Time (sec)')
 
@@ -16,7 +16,7 @@ hold on
 plot(bt,bmeas_pos,'r:','LineWidth',2)
 plot(tout,simout_9b,'b')
 legend('Experimental','Model','Location','best')
-title('9(b) P [10 0 0] Ramp Input')
+title('P [10 0 0] Ramp Input')
 ylabel('Displacement (inches)')
 xlabel('Time (sec)')
 
@@ -27,7 +27,7 @@ hold on
 plot(b2t,b2meas_pos,'r:','LineWidth',2)
 plot(tout,simout_9b2,'b')
 legend('Experimental','Model','Location','best')
-title('9(b2) PI [10 10 0] Ramp Input')
+title('PI [10 10 0] Ramp Input')
 ylabel('Displacement (inches)')
 xlabel('Time (sec)')
 
@@ -38,7 +38,11 @@ hold on
 plot(ct,cmeas_pos,'r:','LineWidth',2)
 plot(tout,simout_9c,'b') 
 legend('Experimental','Model','Location','best')
-title('9(c) PID [42.5 12 0.15] Step Input')
+title('PID [42.5 12 0.15] Step Input')
 ylabel('Displacement (inches)')
 xlabel('Time (sec)')
+
+%% Linear TF Open Loop 
+
+
 
