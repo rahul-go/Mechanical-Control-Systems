11305b0c97c5b33a805852d9a08b0e9d1e42f535
keyannabh <48195436+keyannabh@users.noreply.github.com>
2019-03-10 21:36:17 -0700

    Figure updates

--- Lab 4/HydraulicPositioner/CombinationGraphs.m
+++ Lab 4/HydraulicPositioner/CombinationGraphs.m
@@ -1,3 +1,47 @@
+%% 4.a step P 10 
+
+figure;
+hold on
+plot(at,a4meas_pos,'r:','LineWidth',2)
+plot(tout,simout_4a,'b')
+legend('Experimental','Model','Location','best')
+title('P [10 0 0] Step Input')
+ylabel('Displacement (inches)')
+xlabel('Time (sec)')
+
+%% 4.b step P 20
+
+figure;
+hold on
+plot(at,b4meas_pos,'r:','LineWidth',2)
+plot(tout,simout_4b,'b')
+legend('Experimental','Model','Location','best')
+title('P [20 0 0] Step Input')
+ylabel('Displacement (inches)')
+xlabel('Time (sec)')
+
+%% 4.c step P 37
+
+figure;
+hold on
+plot(at,c4meas_pos,'r:','LineWidth',2)
+plot(tout,simout_4c,'b')
+legend('Experimental','Model','Location','best')
+title('P [37 0 0] Step Input')
+ylabel('Displacement (inches)')
+xlabel('Time (sec)')
+
+%% 4.d step P 82
+
+figure;
+hold on
+plot(tout,d4meas_pos,'r:','LineWidth',2)
+plot(tout,simout_4d,'b')
+legend('Experimental','Model','Location','best')
+title('P [82 0 0] Step Input')
+ylabel('Displacement (inches)')
+xlabel('Time (sec)')
+
 %% 9.a step PD 10 0.01
 
 figure;
